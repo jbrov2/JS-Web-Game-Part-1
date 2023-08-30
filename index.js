f17e470f0d2c358c70f3f2aff2c03523819000f8
zephyr <jafaribrownsha@gmail.com>
2023-08-29 21:29:44 -0400

    finished the actitivty like i was supposed to, tried the sky and got tired lol

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(source, posLeft, postBottom){
+    let imgObj = document.createElement('img')
+    imgObj.src = source
+    imgObj.style.position = 'fixed'
+    imgObj.style.left = posLeft
+    imgObj.style.bottom = postBottom
+    document.body.append(imgObj)
+}
+
+
+newImage('assets/green-character.gif','100px','100px')
+newImage('assets/pine-tree.png','450px','200px')
+newImage('assets/tree.png','200px','300px')
+newImage('assets/pillar.png','350px','100px')
+newImage('assets/crate.png','150px','200px')
+newImage('assets/well.png','500px','425px')
+
+
+function newItem(itemSource,itemLeft,itemBottom){
+    let itemObj = document.createElement('img')
+    itemObj.src = itemSource
+    itemObj.position = 'fixed'
+    itemObj.style.left = itemLeft
+    itemObj.style.bottom = itemBottom
+    document.body.append(itemObj)
+
+    itemObj.addEventListener('dblclick', function(){
+        itemObj.remove()
+
+    })
+}
+
+newItem('assets/sword.png','500px','405px')
+newItem('assets/shield.png','165px','185px')
+newItem('assets/staff.png','600px','100px')
+
+
